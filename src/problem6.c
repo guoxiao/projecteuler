2ef28031b9803309dd110168b51651d3ad1fc66f
Guo Xiao <guoxiao08@gmail.com>
2014-10-02 01:29:24 +0800

    Add problem 6

---
+++ src/problem6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int sum1 = 0, sum2 = 0;
+    for (int i = 1; i <= 100; i++) {
+        sum1 += i * i;
+        sum2 += i;
+    }
+    printf("%d\n", sum2 * sum2 - sum1);
+    return 0;
+
+}
